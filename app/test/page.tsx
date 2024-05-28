6ed4511b8655309e640c3812d3af80b4528d6fcc
Anthony Elliott <anthony.elliott@fiveq.com>
2024-05-28 16:56:19 -0500

    add test page

---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+export default function TestPage() {
+    return (
+        <main>
+            <div>
+                <h1>Test Page</h1>
+                <a href="/">Go Home</a>
+            </div>
+            <div id="journity-embed-container"></div>
+        </main>
+    );
+}
