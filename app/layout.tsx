732e50f3b1342ce70b898e0057a194949ceef20e
Anthony Elliott <anthony.elliott@fiveq.com>
2024-05-28 16:44:54 -0500

    add css file, waypoint looks nice now

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from 'next/script';
 import "../styles/globals.css";
+import '../public/journity.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
